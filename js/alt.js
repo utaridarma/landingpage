d5510b7ac6c9abc1ba1073310616e6c0253cc466
rismapradnyadari <rismapradnyadariii@gmail.com>
2021-07-05 18:53:10 +0800

    landingpage

---
+++ js/alt.js
@@ -0,0 +1,7 @@
+function sidebarOpen() {
+    document.getElementById("sidebar").style.transform = "translate(0px)";
+}
+
+function sidebarClose() {
+    document.getElementById("sidebar").style.transform = "translate(300 px)";
+}
